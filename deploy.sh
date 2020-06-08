90a0af312a38c0b5f45dbe3706e0955c1764b499
anashofiyakurniawati <shofiya@alterra.id>
2020-06-08 11:34:39 +0700

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 sudo docker build -f "Dockerfile" -t anashofiya/fe:$TRAVIS_BUILD_NUMBER .
-sudo docker push ian18ishar/fe:$TRAVIS_BUILD_NUMBER
+sudo docker push anashofiya/fe:$TRAVIS_BUILD_NUMBER
 
 echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
@@ -10,4 +10,4 @@ kubectl config view
 kubectl config current-context
 
  ## deploy api server
-kubectl -n alta5 set image deployment/app-2048 app-2048=anashofiya/fe:$TRAVIS_BUILD_NUMBER
+kubectl -n alta5 set image deployment/reak reak=anashofiya/react-tutorial:$TRAVIS_BUILD_NUMBER
