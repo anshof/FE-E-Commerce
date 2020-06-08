98a7f1be86611d207f6f28d1618a46f4a7cc8d43
anashofiyakurniawati <shofiya@alterra.id>
2020-06-08 11:05:49 +0700

    add deploy.sh and travis.yaml

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+sudo docker build -f "Dockerfile" -t ian18ishar/fe:$TRAVIS_BUILD_NUMBER .
+sudo docker push ian18ishar/fe:$TRAVIS_BUILD_NUMBER
+
+echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud container clusters get-credentials $CLUSTER_NAME_PRD --region $CLOUDSDK_COMPUTE_REGION --project $PROJECT_NAME_PRD
+
+kubectl config view
+kubectl config current-context
+
+ ## deploy api server
+kubectl -n ian set image deployment/app-2048 app-2048=ian18ishar/fe:$TRAVIS_BUILD_NUMBER
